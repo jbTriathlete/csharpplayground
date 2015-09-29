574166ccd13d5f5c90c78303beddf95e30cd58bd
Jon Baker <jonbakerphoto@gmail.com>
2015-09-28 20:20:01 -0500

    C # Fun

---
+++ csharpplayground/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csharpplayground
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[,] array ={ {"a","s","r","t"},{"3","4","6","7"}};
+            for (int i = 0; i <array.GetLength(0); i++)
+            {
+                for (int j=0; j<array.GetLength(1);j++)
+                {
+                    Console.WriteLine(array[i, j]);
+                    textBox1.Text = textBox1.Text + array[i,j];
+                }
+
+            }
+
+           // string[][] array2 = { { "a", "s", "r", "t" }},{{ "3", "4", "6", "7" } };
+       
+
+         
+           
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Transaction t1 = new Transaction("001", "9/23/2015", 1000000);
+
+            textBox1.Text = t1.showTransaction();
+        }
+    }
+}
---
+++ csharpplayground/car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpplayground
+{
+    interface ITransaction
+    {
+       string showTransaction();
+       double getAmount();
+    }
+
+    public class Transaction:ITransaction
+    {
+        private string tCode;
+        private string date;
+        private double amount;
+
+        public Transaction()
+        {
+            tCode = "";
+            date = "";
+            amount = 0.0;
+        }
+
+        public Transaction(string inCode, string inDt, double inAmt)
+        {
+            tCode = inCode;
+            date = inDt;
+            amount = inAmt;
+        }
+
+        public double getAmount()
+        {
+            return amount;
+        }
+
+        public string showTransaction()
+        {
+            string str;
+            str = "Transaction:" + tCode + System.Environment.NewLine;
+            str = str + "Date:" + date + System.Environment.NewLine;
+            str = str + "Amount:" + amount + System.Environment.NewLine;
+            return str;
+        }
+    }
+}
